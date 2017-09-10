f9f6410fda1fab4137554ff92e064a34ac7de2be
peteratskoruz <peterjoeeraj234@gmail.com>
2017-09-10 18:08:19 +0530

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { RootComponent } from './app.component';
+import { HighLighterDirective } from './high-lighter.directive';
+
+@NgModule({
+  declarations: [
+    RootComponent,
+    HighLighterDirective
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [RootComponent]
+})
+export class AppModule { }
